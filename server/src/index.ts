48e247c8d653aca7648334db7adcf6002fadd6c4
AngerOverApathy <violasian@gmail.com>
2023-12-28 15:43:13 -0800

    added fucntionality comments to code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,20 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { userRouter } from './routes/user';
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 
-const dbPassword = process.env.DB_PASSWORD;
-const app = express();
+const dbPassword = process.env.DB_PASSWORD; // Retrieve database password from environment variables
+const app = express(); // Create an Express application
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors()); // Middleware to enable CORS (Cross-Origin Resource Sharing)
 
+// Register the user routes with the application
 app.use('/user', userRouter);
 
+// Connect to MongoDB using the connection string
 mongoose.connect(`mongodb+srv://vee:${dbPassword}@vsza-tech.kk0y3cx.mongodb.net/vsza-tech`)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('Connected to MongoDB')) 
+  .catch(err => console.error('Error connecting to MongoDB:', err)); 
 
+// Start the server on port 3001
 app.listen(3001, () => {
-    console.log('Server started')
+    console.log('Server started') // Log a message when the server starts successfully
 })
--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,18 +1,21 @@
 import { Schema, model } from 'mongoose';
 
+// Define an interface for User data structure
 export interface IUser {
-    _id?: string
-    username: string;
-    password: string;
-    availableMoney: number;
-    //purchasedItems: string[];
+    _id?: string; // Optional MongoDB document ID
+    username: string; // User's username
+    password: string; // User's password
+    availableMoney: number; // User's available money
+    // purchasedItems: string[]; // Array of purchased item identifiers
 }
 
+// Define the User schema that maps to the MongoDB collection
 const UserSchema = new Schema<IUser>({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true},
-    availableMoney: { type: Number, required: true, default: 5000 },
-    //purchasedItems: { type: Array, required: true, default: [] }
+    username: { type: String, required: true, unique: true }, // Username must be unique and is required
+    password: { type: String, required: true }, // Password is required
+    availableMoney: { type: Number, required: true, default: 5000 }, // Available money with a default value of 5000
+    // purchasedItems: { type: Array, required: true, default: [] } // Array of purchased items, required with a default empty array
 });
 
-export const UserModel = model<IUser>('user', UserSchema);
+// Create a model from the schema to interact with the 'user' collection
+export const UserModel = model<IUser>('user', UserSchema); // 'user' is the name of the collection in MongoDB
