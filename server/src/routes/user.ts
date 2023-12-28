bc5cb3814d63fd9b9afb3296281579d4165e2439
AngerOverApathy <violasian@gmail.com>
2023-12-28 14:30:18 -0800

    added register route

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -7,14 +7,21 @@ const router = Router();
 
 router.post("/register", async (req: Request, res: Response) => {
     const {username, password} = req.body;
+    try{
+        const user = await UserModel.findOne({ username })
 
-    const user = await UserModel.findOne({ username })
+        if (user) {
+            return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+        }
 
-    if (user) {
-        return res.status(400).json({ type: UserErrors.USERNAME_ALREADY_EXISTS });
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new UserModel({ username, password: hashedPassword });
+        await newUser.save();
+
+        return res.json({ message: 'User registered successfully' });
+    } catch (err) {
+        return res.status(500).json({ type: err, message: err.message });
     }
-
-    const newUser = new UserModel({ username, password });
 });
 
 export { router as userRouter };
