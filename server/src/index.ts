7e73c3e3a19ca45a44a5a1b12eb780e29bf95d81
AngerOverApathy <violasian@gmail.com>
2023-12-27 18:42:16 -0800

    fixed env

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+
+require('dotenv').config();
+const dbPassword = process.env.DB_PASSWORD;
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    "mongodb+srv://vee:<password>@vsza-tech.kk0y3cx.mongodb.net/"
+    `mongodb+srv://vee:${dbPassword}@vsza-tech.kk0y3cx.mongodb.net/`
 );
 
 app.listen(3000, () => {console.log('Server started')})
