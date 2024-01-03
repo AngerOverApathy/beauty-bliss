f6146e1fbdf92cd50a89ac7be4cb1f756d9469d2
AngerOverApathy <violasian@gmail.com>
2024-01-03 13:00:11 -0800

    auth code complete

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'; 
 
 export const Navbar = () => {
     return (
@@ -8,6 +10,9 @@ export const Navbar = () => {
             </div>
 
             <div className="navbar-links">
+                <Link to='/'>Shop</Link>
+                <Link to='/purchased-items'>Purchases</Link>
+                <Link to='/checkout'> <FontAwesomeIcon icon= </Link>
                 <Link to='/'>Shop</Link>
             </div>
         </div>
