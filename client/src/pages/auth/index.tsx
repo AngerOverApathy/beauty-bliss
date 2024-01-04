4c4e125554ac0eb64e4a5e525c05484afd041235
AngerOverApathy <violasian@gmail.com>
2024-01-04 15:45:56 -0800

    registration working

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,3 +1,7 @@
+import { useState, SyntheticEvent } from 'react';
+import axios from 'axios';
+import { UserErrors } from '../../errors';
+
 export const AuthPage = () => {
     return (
         <div className='auth'>
@@ -8,6 +12,27 @@ export const AuthPage = () => {
 }
 
 const Register = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (event: SyntheticEvent) => {
+        event.preventDefault()
+        try {
+            await axios.post('http://localhost:3001/user/register', {
+                username, 
+                password
+            })
+            alert('User registered, now login!')
+        } catch (err) {
+            if (err?.response?.data?.type === UserErrors.USERNAME_ALREADY_EXISTS) {
+                alert('ERROR: Username already exists')
+            } else {
+                alert('ERROR: Something went wrong')
+            }
+        }
+
+    }
+
     return (
         <div className='auth-container'>
           
@@ -15,14 +40,24 @@ const Register = () => {
                 <h2>Register</h2>
                 <div className="form-group">
                     <label htmlFor="username">Username: </label>
-                    <input type="text" id="username"/>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="password">Password: </label>
-                    <input type="password" id="password"/>
+                    <input 
+                        type="password" 
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
                 </div> 
-                <button type='submit'>Register</button>'   
+                <button type='submit' onClick={handleSubmit}>Register</button>   
             </form>
         </div>
     )
