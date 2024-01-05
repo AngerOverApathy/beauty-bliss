9c5961bb2e58fea98f8c9a6118fc86dbf0e18094
AngerOverApathy <violasian@gmail.com>
2024-01-04 16:12:43 -0800

    login created

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,6 +1,7 @@
 import { useState, SyntheticEvent } from 'react';
-import axios from 'axios';
 import { UserErrors } from '../../errors';
+import { useCookies } from 'react-cookie';
+import axios from 'axios';
 
 export const AuthPage = () => {
     return (
@@ -12,8 +13,8 @@ export const AuthPage = () => {
 }
 
 const Register = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
 
     const handleSubmit = async (event: SyntheticEvent) => {
         event.preventDefault()
@@ -64,7 +65,63 @@ const Register = () => {
 }
 
 const Login = () => {
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [_, setCookies] = useCookies(['access_token'])
+
+    const handleSubmit = async (event: SyntheticEvent) => {
+        event.preventDefault()
+        try {
+            const result = await axios.post('http://localhost:3001/user/login', {
+                username, 
+                password
+            })
+            setCookies('access_token', result.data.access_token)
+            localStorage.setItem('userID', result.data.userID)
+        } catch (err) {
+
+            let errorMessage: string = ''
+            switch (err?.response?.data?.type) {
+                case UserErrors.NO_USER_FOUND:
+                    errorMessage = 'ERROR: Username not found'
+                    break
+                case UserErrors.WRONG_CREDENTIALS:
+                    errorMessage = 'ERROR: Incorrect password/username'
+                    break
+                default:
+                    errorMessage = 'ERROR: Something went wrong'
+            }
+            alert(errorMessage)
+        }
+
+    }
+
     return (
-        <div>Login</div>
+        <div className='auth-container'>
+          
+            <form>
+                <h2>Login</h2>
+                <div className="form-group">
+                    <label htmlFor="username">Username: </label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="password">Password: </label>
+                    <input 
+                        type="password" 
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div> 
+                <button type='submit' onClick={handleSubmit}>Login</button>   
+            </form>
+        </div>
     )
 }
